a43aab3a330ce4c7056ba09d23773036a7749218
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-17 07:14:45 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+  my_hash = {}
+  
+  languages.each do |style, hash|
+    
+      hash.collect do |language, type_hash|
+        if my_hash.has_key?(language)
+          my_hash[language][:style] << style
+        else
+          my_hash[language] = type_hash
+          my_hash[language][:style] = [style]
+        end
+      end
+    
+  end
+
+  my_hash
 end
