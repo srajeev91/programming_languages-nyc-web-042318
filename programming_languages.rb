dd77d2d7d44266df3472487503a3e9290e1a1acf
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-17 06:56:23 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,9 @@ def reformat_languages(languages)
   hash = {}
   
   languages.each do |style, hash|
-    if style == :oo
+    
       hash.collect do |language, type_hash|
+        if hash.include
         hash[language] = type_hash
         hash[language][:style] = [style]
       end
