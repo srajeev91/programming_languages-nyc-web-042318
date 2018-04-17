ef5ac8e200afbaffe2407971d9c02a26bb02dbea
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-17 07:04:40 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@ def reformat_languages(languages)
     
       hash.collect do |language, type_hash|
         if hash.keys.include?(language)
-          hash[language][:style].push([style])
+          hash[language][:style] << [style]
         else
           hash[language] = type_hash
           hash[language][:style] = style
