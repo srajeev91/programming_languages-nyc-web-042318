c25cd64c57120e0c60b32a325bf48beae972bee6
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-17 07:03:06 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,7 +8,7 @@ def reformat_languages(languages)
           hash[language][:style] << [style]
         else
           hash[language] = type_hash
-          hash[language]{:style] = style
+          hash[language][:style] = style
         end
       end
     
